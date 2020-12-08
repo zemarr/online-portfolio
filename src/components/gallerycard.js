db90fe00749dcc7851e5ab2c22dc6024daa43fc4
Chukwudalu Ayika <chukwudaluay@gmail.com>
2020-12-08 14:59:59 +0100

    add projects

--- src/components/gallerycard.js
+++ src/components/gallerycard.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types'
 import style from './galleryCard.module.css'
 
-const GalleryCard = ({ frontImageClass, projectName, backDetails }) => {
+const GalleryCard = ({ frontImageClass, projectName, projectDescription, tools, visit, linkHandle }) => {
     return (
         <div className={style.card}>
-            <div class={`${style.front} ${frontImageClass}`}>
+            <div className={`${style.front} ${frontImageClass}`}>
                 <div className={style.overlay}>
                     <h4>{projectName}</h4>
                 </div>
             </div>
-            <div class={`${style.back}`}>
+            <div className={`${style.back}`}>
                 <h5>{projectName}</h5>
+                <p>{projectDescription}</p>
+                <p><i>{tools}</i></p>
+                <a href={visit}
+                    rel='noreferrer'
+                    target='_blank'>{linkHandle}</a>
             </div>
         </div>
     );
+}
+
+GalleryCard.propTypes = {
+    projectName: PropTypes.string.isRequired,
+    projectDescription: PropTypes.string.isRequired,
+    tools: PropTypes.string.isRequired,
+    linkHandle: PropTypes.string.isRequired
 }
 
 
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -4,21 +4,49 @@ import style from './projects.module.css'
 import Layout from '../components/layout'
 import GalleryCard from '../components/gallerycard'
 
-const projects = () => {
+const Projects = () => {
     return (
         <Layout>
             <section className={style.projectGallery}>
                 <div className={style.galley}>
-                    <GalleryCard frontImageClass={style.wickretMockup} projectName="Wickret" />
+                    <GalleryCard
+                        frontImageClass={style.wickretMockup}
+                        projectName="Wickret"
+                        projectDescription='A landing page of Wickret I cloned from scratch. 
+                        Wickret is Modern online banking website I found very visually appealing. 
+                        I was driven to test myself and recreate it.'
+                        tools='Tools: HTML | CSS | JAVASCRIPT'
+                        visit='https://zemarr.github.io/Wickret-landing-page/'
+                        linkHandle="Visit"
+                    />
+                    <GalleryCard
+                        frontImageClass={style.busvestMockup}
+                        projectName="BusVest"
+                        projectDescription='BusVest is an innovative digital platform that gives 
+                        everyone the opportunity to generate revenue from public transportation 
+                        anytime and anywhere by funding buses.'
+                        tools='Tools: HTML | CSS | JAVASCRIPT'
+                        visit='https://busvest.co/'
+                        linkHandle="Invest in Public tranportation"
+                    />
+                    <GalleryCard
+                        frontImageClass={style.XOMockup}
+                        projectName="Tic-Tac-Toe"
+                        projectDescription='A Javascript game.
+
+                        Take turns to fill the boxes until a player wins'
+                        tools='Tools: JAVASCRIPT'
+                        visit='http://bit.ly/2TkX-0u0JG'
+                        linkHandle="Play now"
+                    />
+
+                    {/* <GalleryCard />
                     <GalleryCard />
-                    <GalleryCard />
-                    <GalleryCard />
-                    <GalleryCard />
-                    <GalleryCard />
+                    <GalleryCard /> */}
                 </div>
             </section >
         </Layout >
     );
 }
 
-export default projects;
+export default Projects;
